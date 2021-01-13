36d801ae6cd6f87793bd4deb9d3754e988fd90ac
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-01-13 15:50:53 +0300

    пакеты по минификации html&js; задача reload

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
-// const htmlmin = require("gulp-htmlmin");
-const uglify = require("gulp-uglify-es");
+const htmlmin = require("gulp-htmlmin");
+const terser = require("gulp-terser");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
@@ -37,7 +37,7 @@ exports.styles = styles;
 
 const html = () => {
   return gulp.src("source/**/*.html")
-  // .pipe(htmlmin({collapseWhitespace:true}))
+  .pipe(htmlmin({collapseWhitespace:true}))
   .pipe(gulp.dest("build"))
 }
 
@@ -46,9 +46,9 @@ exports.html = html;
 // Scripts
 
 const scripts = () => {
-  return gulp.src("source/js/*.js")
-    // .pipe(uglify())
-    // .pipe(rename("script.min.js"))
+  return gulp.src("source/js/script.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
     .pipe(gulp.dest("build/js"))
     .pipe(sync.stream());
 }
@@ -127,11 +127,20 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
+exports.reload = reload;
+
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html", gulp.series(html, sync.reload));
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 // Build
