8d56fa993e0c61153e5f459a52e97ff90729159f
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-01-14 14:16:49 +0300

    добавила в сборку  задачу images

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,7 @@ exports.sprite = sprite;
 const copy = () => {
   return gulp.src([
     "source/fonts/*.{woff2,woff}",
-    "source/img/**/*.{jpg,png,svg}"
+    // "source/img/**/*.{jpg,png,svg}"
   ],
     {
       base: "source"
@@ -167,6 +167,7 @@ exports.default = gulp.series(
     html,
     scripts,
     copy,
+    images,
     createWebp
   ),
   gulp.series(
